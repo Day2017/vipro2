540b8af69a580afa5aa5a1f40678a9362937650e
Natphol Niemsaku <kanjirokoong2012@gmail.com>
2018-03-28 17:33:58 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -22,7 +22,7 @@ class Talk:
 
   UA = "Line/1.4.17"
 #  LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
-  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
+  LA = "CHROMEOS\t7.18.0\tChrome_OS\t1"
 
   authToken = None
   cert = None
